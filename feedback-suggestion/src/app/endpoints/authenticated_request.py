5b9346252df211369924d4122bdac34f91ed2488
botrufus <64660684+botrufus@users.noreply.github.com>
2023-01-14 13:49:17 +0100

    add authenticated request

---
+++ feedback-suggestion/src/app/endpoints/authenticated_request.py
@@ -0,0 +1,50 @@
+from packaging import version
+import requests
+
+class AuthRequest:
+
+    headers = { "Content-type": "application/json" }
+    cookies = {}
+
+    def __init__(self, token, server):
+        assert server[-1] != "/" 
+        if self.get_artemis_version(server) < version.parse("6.0.0"):
+            self.headers["Authorization"] = "Bearer " + token
+        else:
+            self.cookies = { "jwt": token }
+        self.server = server
+        self.api_path = self.server + "/api"
+    
+    @staticmethod
+    def get_artemis_version(server):
+        response = requests.get(server)
+        if not response.ok:
+            raise ConnectionError("Connection to artemis server failed")
+        if hasattr(response.headers, "Content-Version"):
+            return version.parse(response.headers["Content-Version"])
+        else:
+            raise AttributeError("No Content-Version attribute in response headers")
+    
+    def get(self, path, params = None):
+        if params is None:
+            params = {}
+        return requests.get(self.api_path + path, headers=self.headers, cookies=self.cookies, params=params)
+    
+    def post(self, path, params = None, body=None):
+        if params is None:
+            params = {}
+        if body is None:
+            body = {}
+        return requests.post(self.api_path + path, headers=self.headers, cookies=self.cookies, params=params, json=body)
+
+    def put(self, path, params = None, body=None):
+        if params is None:
+            params = {}
+        if body is None:
+            body = {}
+        return requests.put(self.api_path + path, headers=self.headers, cookies=self.cookies, params=params, json=body)
+    
+    def delete(self, path, params = None):
+        if params is None:
+            params = {}
+        return requests.delete(self.api_path + path, headers=self.headers, cookies=self.cookies, params=params)
