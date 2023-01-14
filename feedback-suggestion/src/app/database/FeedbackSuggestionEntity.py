6c5212c0bb577c11f4c15c485e223c66d435a4b9
Paul Schwind <pal03377@users.noreply.github.com>
2023-01-14 13:23:08 +0100

    Use sqlalchemy (#3)

--- feedback-suggestion/src/app/database/FeedbackSuggestionEntity.py
+++ feedback-suggestion/src/app/database/FeedbackSuggestionEntity.py
@@ -1,45 +1,28 @@
-import psycopg2
 import os
+
+from sqlalchemy import create_engine
 
 
 class FeedbackSuggestionEntity:
 
     def __init__(self):
-        DB_HOST = str(os.environ['DATABASE_HOST']) if "DATABASE_HOST" in os.environ else "postgres"
-        DB_PORT = int(os.environ['DATABASE_PORT']) if "DATABASE_PORT" in os.environ else "5432"
-        DB_NAME = str(os.environ['DATABASE_NAME']) if "DATABASE_NAME" in os.environ else "feedback_suggestion_db"
-        DB_USER = str(os.environ['DATABASE_USER']) if "DATABASE_USER" in os.environ else "postgres_user"
-        DB_PASS = str(os.environ['DATABASE_PASS']) if "DATABASE_PASS" in os.environ else "password"
-
-        try:
-            self.conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host="0.0.0.0", port=DB_PORT)
-            print("Database connected successfully")
-        except:
-            print(DB_HOST)
-            print(DB_PORT)
-            print(DB_NAME)
-            print(DB_USER)
-            print(DB_PASS)
-            print("Database not connected successfully")
+        DB_USER = os.environ.get('DATABASE_USER', 'feedback_suggestion_user')
+        DB_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'password')
+        DB_HOST = os.environ.get('DATABASE_HOST', 'localhost')
+        DB_PORT = os.environ.get('DATABASE_PORT', '5432')
+        DB_NAME = os.environ.get('DATABASE_NAME', 'feedback_suggestion_db')
+        db_link = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+        self.engine = create_engine(db_link)
 
     def insert_name(self, first_name, last_name, email):
-        cur = self.conn.cursor()
-        cur.execute("INSERT INTO developers (FIRST_NAME,LAST_NAME,EMAIL) VALUES(%s, %s, %s)",
-                    (first_name, last_name, email))
-                    
-        print('Data inserted successfully')
-        self.conn.commit()
-        self.conn.close()
+        with self.engine.connect() as conn:
+            conn.execute("INSERT INTO developers (first_name, last_name, email) VALUES (%s, %s, %s)", (first_name, last_name, email))
+            print('Data inserted successfully')
 
     def fetch_name(self):
-        cur = self.conn.cursor()
-        cur.execute("SELECT * FROM developers")
-        cur.fetchone()
-        for record in cur:
-            print(record)
-
-        print('Data fetched successfully')
-        self.conn.commit()
-        self.conn.close()
-
-        return cur
+        with self.engine.connect() as conn:
+            result = conn.execute("SELECT * FROM developers")
+            all_devs = result.fetchall()
+            print('Data fetched successfully')
+            print(all_devs)
+            return all_devs
