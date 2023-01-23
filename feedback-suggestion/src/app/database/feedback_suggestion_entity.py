57b07b04587ec225c38fd8edcac461b6c77d7806
Florian Huber <florian.maximilian.huber@tum.de>
2023-01-23 22:13:08 +0100

    Remove unnecessary exmaple code

--- feedback-suggestion/src/app/database/feedback_suggestion_entity.py
+++ feedback-suggestion/src/app/database/feedback_suggestion_entity.py
@@ -15,20 +15,6 @@ class FeedbackSuggestionEntity:
         db_link = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
         self.engine = create_engine(db_link)
 
-    def insert_name(self, first_name, last_name, email):
-        with self.engine.connect() as conn:
-            conn.execute("INSERT INTO developers (first_name, last_name, email) VALUES (%s, %s, %s)",
-                         (first_name, last_name, email))
-            print('Data inserted successfully')
-
-    def fetch_name(self):
-        with self.engine.connect() as conn:
-            result = conn.execute("SELECT * FROM developers")
-            all_devs = result.fetchall()
-            print('Data fetched successfully')
-            print(all_devs)
-            return all_devs
-
     def fetch_feedbacks(self):
         with self.engine.connect() as conn:
             result = conn.execute("SELECT * FROM feedbacks")
