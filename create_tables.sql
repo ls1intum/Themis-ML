99625f01af54e1f720ab749fb0554d933f1361b1
Florian Huber <florian.maximilian.huber@tum.de>
2023-01-23 22:16:07 +0100

    Remove unnecessary example database table

--- create_tables.sql
+++ create_tables.sql
@@ -1,13 +1,5 @@
 GRANT ALL PRIVILEGES ON DATABASE feedback_suggestion_db TO feedback_suggestion_user;
 
-CREATE TABLE IF NOT EXISTS developers (
-  DEVELOPER_ID SERIAL,
-  FIRST_NAME varchar(250) NOT NULL,
-  LAST_NAME varchar(250) NOT NULL,
-  EMAIL varchar(250),
-  PRIMARY KEY (DEVELOPER_ID)
-);
-
 CREATE TABLE IF NOT EXISTS feedbacks (
   id SERIAL PRIMARY KEY,
   exercise_id INT NOT NULL,
