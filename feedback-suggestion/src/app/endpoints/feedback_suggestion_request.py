2ad50be68951163b8b00b2cdc7b15024cb86a625
Paul Schwind <git@pschwind.de>
2023-01-25 02:05:30 +0100

    prevent feedback duplication

--- feedback-suggestion/src/app/endpoints/feedback_suggestion_request.py
+++ feedback-suggestion/src/app/endpoints/feedback_suggestion_request.py
@@ -50,6 +50,7 @@ def load_feedbacks(request: NotifyRequest):
     logger.info("Notified about new feedback!")
 
     auth_request = AuthRequest(request.token, request.server)
+    # Make sure to not reveal sensitive information before this request!
     response = auth_request.get(f"/repository/{request.participation_id}/files-content")
     files: dict = {name: content for name, content in response.json().items() if name.endswith(".java")}
 
@@ -82,6 +83,8 @@ def load_feedbacks(request: NotifyRequest):
                             credits=feedback.credits
                         )
                     )
+    # remove feedbacks for this participation before inserting new ones
+    db.delete_feedbacks(request.participation_id)
     db.store_feedbacks(method_feedbacks)
 
     return "Success!"
